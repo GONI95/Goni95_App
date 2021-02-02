750ebc19bef2fa08baf985f16936e51fd64c9847
GONI95 <sanggoni009@gmail.com>
2021-02-02 18:52:29 +0900

    create recyclerView item layout

--- app/src/main/java/com/example/goni95_app/recyclerview/PhotoGridRecyclerViewAdapter.kt
+++ app/src/main/java/com/example/goni95_app/recyclerview/PhotoGridRecyclerViewAdapter.kt
@@ -3,5 +3,16 @@ package com.example.goni95_app.recyclerview
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 
-class PhotoGridRecyclerViewAdapter  {
+class PhotoGridRecyclerViewAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
 }
--- app/src/main/java/com/example/goni95_app/recyclerview/PhotoItemViewHolder.kt
+++ app/src/main/java/com/example/goni95_app/recyclerview/PhotoItemViewHolder.kt
@@ -1,4 +1,8 @@
 package com.example.goni95_app.recyclerview
 
-class PhotoItemViewHolder {
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+class PhotoItemViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView) {
+    //포토아이템뷰홀더가 생성될 때 itemVIew를 생성자로 받아 상속받는 ViewHolder에 필요한 view를 넣어준다.
 }

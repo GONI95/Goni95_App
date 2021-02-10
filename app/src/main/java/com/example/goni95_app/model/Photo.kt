aabe83488845fd156766df1cf7dec3bc44f2deec
GONI95 <sanggoni009@gmail.com>
2021-02-10 16:20:00 +0900

    파싱

--- app/src/main/java/com/example/goni95_app/model/Photo.kt
+++ app/src/main/java/com/example/goni95_app/model/Photo.kt
@@ -1,4 +1,7 @@
 package com.example.goni95_app.model
 
-class Photo {
-}
+import java.io.Serializable
+
+//생성자
+data class Photo(var thumbnail: String?, var author: String?, var createdAt: String?, var likesCount: Int?) :Serializable
+// Serializable을 상속받아 직렬화가 가능하도록 한다.
--- app/src/main/java/com/example/goni95_app/viewModel/HomeViewModel.kt
+++ app/src/main/java/com/example/goni95_app/viewModel/HomeViewModel.kt
@@ -3,6 +3,7 @@ package com.example.goni95_app.viewModel
 import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.goni95_app.model.Photo
 import com.example.goni95_app.retrofit.IRetrofit_Service
 import com.example.goni95_app.retrofit.RetrofitClient
 import com.example.goni95_app.util.*
@@ -14,6 +15,7 @@ import org.json.JSONObject
 import retrofit2.Call
 import retrofit2.Response
 import java.lang.Exception
+import java.text.SimpleDateFormat
 
 class HomeViewModel : ViewModel() {
     // http call 생성
@@ -27,17 +29,74 @@ class HomeViewModel : ViewModel() {
 
 
     //사진검색 api 호출
-    fun searchPhotos(searchTerm: String?, completion: (RESPONSE_STATE, String) -> Unit){
+    fun searchPhotos(searchTerm: String?, completion: (RESPONSE_STATE, String) -> Unit) {
         val term = searchTerm.let { it } ?: ""
 
-        viewModelScope.launch(Dispatchers.IO){
-            val call = iretrofitService?.serachPhotos(searchTerm = term).let { it.toString() } ?: return@launch
-            Log.d(Constants.TAG, "${HomeViewModel::class.java.simpleName} / result : ${JSONObject(call).toString(4)}")
-            
-            completion(RESPONSE_STATE.OK, JSONObject(call).toString(4))
+        viewModelScope.launch(Dispatchers.IO) {
+            val call = iretrofitService?.serachPhotos(searchTerm = term).let { it } ?: return@launch
+
+            var parsedPhotoDataArray = ArrayList<Photo>()
+
+            val body = call.asJsonObject
+            val results = body.getAsJsonArray("results")
+            val total = body.get("total").asInt
+
+            Log.d(Constants.TAG, "${HomeViewModel::class.java.simpleName} " +
+                    "/ message : ${total}")
+
+            if (total != 0) {
+                // JsonArray result[] 내부의 JsonObject를 하나씩 받아 반복 수행
+                results.forEach { resultItem ->
+                    val resultItemObject = resultItem.asJsonObject // get object
+                    val user =
+                        resultItemObject.get("user").asJsonObject    // object -> user
+                    val username = user.get("username").asString    // user -> username
+                    val likeCount =
+                        resultItemObject.get("likes").asInt     // object -> likes
+                    val thumbnail =
+                        resultItemObject.get("urls").asJsonObject.get("thumb").asString      // urls -> thumb
+                    val createdAt =
+                        resultItemObject.get("created_at").asString  // object -> create_at
+
+                    Log.d(Constants.TAG, "${HomeViewModel::class.java.simpleName} " +
+                            "/ message : ${user}")
+                    Log.d(Constants.TAG, "${HomeViewModel::class.java.simpleName} " +
+                            "/ message : ${username}")
+                    Log.d(Constants.TAG, "${HomeViewModel::class.java.simpleName} " +
+                            "/ message : ${likeCount}")
+                    Log.d(Constants.TAG, "${HomeViewModel::class.java.simpleName} " +
+                            "/ message : ${thumbnail}")
+                    Log.d(Constants.TAG, "${HomeViewModel::class.java.simpleName} " +
+                            "/ message : ${createdAt}")
+
+
+
+                    val parser = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss")
+                    // 실제 Date 형태
+                    val formatter = SimpleDateFormat("yyyy년\nMM월 dd일")
+                    // 변경하려는 Date 형태
+
+                    val outputDateString = formatter.format(parser.parse(createdAt))
+                    // Date를 원하는 형태로 치환
+
+                    //Log.d(Constants.TAG, "RetrofitManager - onResponse() called / outputDateString : ${outputDateString}")
+
+                    val photoItem = Photo(
+                        author = username,
+                        likesCount = likeCount,
+                        thumbnail = thumbnail,
+                        createdAt = outputDateString
+                    )
+
+                    parsedPhotoDataArray.add(photoItem)
+                    //ArrayList에 Photo 타입의 데이터를 추가가
+                }
+
+
+                //completion(RESPONSE_STATE.OK, JSONObject(call.asString).toString(4))
+            }
         }
     }
-
 }
 
 
